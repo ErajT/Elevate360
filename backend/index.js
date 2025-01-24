d585d80ea52504cf20df89e36362f7bae29131c1
Eraj Tanweer <erajtanweer2@gmail.com>
2025-01-24 09:34:19 +0500

    we

---
+++ backend/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// const { checkToken } = require("./auth/token_validation")
+
+// const userRouter = require('./api/users/user.router');
+// const LeaderboardRouter = require('./Routes/LeaderboardRoutes');
+// const AdminRouter = require('./Routes/AdminRoutes');
+// const ScheduleRouter = require('./Routes/ScheduledRoutes');
+
+let app = express();
+app.options('*', cors()); // Allow preflight requests
+
+// // Middleware to enable CORS
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://learn-lime-three.vercel.app'); // Adjust the origin as needed
+//   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); 
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true'); 
+//   next();
+// });
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://learn-lime-three.vercel.app'], // Allow requests from frontend application
+  // origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true // Allow credentials (cookies) to be included with requests
+}));
+
+app.use(bodyParser.json({ limit: '200mb' }))
+app.use(bodyParser.urlencoded({ limit: '200mb', extended: true }));
+
+
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://learn-lime-three.vercel.app'], // Allowed origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allowed methods
+  credentials: true // Allow credentials
+};
+
+// Global CORS middleware
+// app.use(cors(corsOptions));
+
+// // Handle preflight requests explicitly for critical routes
+// app.options('/users', cors(corsOptions));
+// app.options('/leaderboard', cors(corsOptions));
+// app.options('/admin', cors(corsOptions));
+// app.options('/schedule', cors(corsOptions));
+
+//middleware for request body
+app.use(express.json());
+// app.use('/users', userRouter);
+// app.use('/leaderboard', checkToken , LeaderboardRouter);
+// app.use('/admin',checkToken, AdminRouter);
+// app.use('/schedule', ScheduleRouter);
